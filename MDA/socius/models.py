c579f895b53ce2ebd3aebf9763b41571c61dfece
Singuru Abhiram <62535575+Abhi-hue5123@users.noreply.github.com>
2020-08-09 10:50:16 +0530

    Initial commit

---
+++ MDA/socius/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Destination(models.Model):  
+    
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics',default='destination_4.jpg')
+    desc = models.TextField(default='Defualt Value')
+    price = models.IntegerField(default=0)
+    offer = models.BooleanField(default=False)
+
+class UserList(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=254,blank=True)
+    coupon = models.CharField(max_length=100, blank=True, null=True)
